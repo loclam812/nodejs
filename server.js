d655526b772e2646796fd3d024a16b4480316f6a
loclam812 <loclam812@gmail.com>
2021-06-30 23:18:04 +0700

    - Update

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const BodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 8080
-const url = "mongodb://localhost:27017/information_employee"
+const url = "mongodb+srv://loclam812:Abc@12345678@cluster0.nxpqw.mongodb.net/information_employee?retryWrites=true&w=majority"
 const corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200
 }
 
-Mongoose.connect(url);
+Mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(BodyParser.json())
 app.use(BodyParser.urlencoded({ extended: true }))
 
@@ -20,17 +20,7 @@ const Information = Mongoose.model("information", {
   data: Array
 });
 
-app.get('/get-information', cors(corsOptions), async (req, res) => {
-  try {
-    const result = await Information.find().exec()
-
-    res.send(result[0].data)
-  } catch (error) {
-    res.status(500).send(error)
-  }
-})
-
-app.get('/get-information-1', async (req, res) => {
+app.get('/get-information', async (req, res) => {
   try {
     const result = await Information.find().exec()
 
